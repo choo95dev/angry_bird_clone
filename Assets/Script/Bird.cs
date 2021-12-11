b7a28b28fe5f86f32daf0adf38b473dfec674837
Choo <choo.chenwei95dev@gmail.com>
2021-12-11 18:08:50 +0800

    Added Bird script

---
+++ Assets/Script/Bird.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+
+    private void OnMouseDown()
+    {
+        GetComponent<SpriteRenderer>().color = Color.red;
+    }
+
+    private void OnMouseUp()
+    {
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
+}

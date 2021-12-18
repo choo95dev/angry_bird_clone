a5323facc8726dcdf4333b939ae5b684463eaee4
Choo <choo.chenwei95dev@gmail.com>
2021-12-18 19:21:27 +0800

    Added onMouseDrag and Update logic for bird

--- Assets/Script/Bird.cs
+++ Assets/Script/Bird.cs
@@ -1,7 +1,24 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Bird : MonoBehaviour
 {
+    private Vector3 _initialPos;
+    [SerializeField] private float _pushForce = 100f;
+
+    private void Awake()
+    {
+        //save starting pos
+        _initialPos = transform.position;
+    }
+
+    private void Update()
+    {
+        if(transform.position.y > 10 || transform.position.y < -10 || transform.position.x > 10 || transform.position.x < -10)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
 
     private void OnMouseDown()
     {
@@ -11,5 +28,15 @@ public class Bird : MonoBehaviour
     private void OnMouseUp()
     {
         GetComponent<SpriteRenderer>().color = Color.white;
+        Vector2 directionToInitialPos = _initialPos - transform.position;
+        GetComponent<Rigidbody2D>().AddForce(directionToInitialPos * _pushForce);
+        GetComponent<Rigidbody2D>().gravityScale = 1;
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector3 newPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        newPos.z = 0;
+        transform.position = newPos;
     }
 }
